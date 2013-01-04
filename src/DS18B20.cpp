36852ee78f603993b28297264468c39a5a801742
Georgi Todorov <terahz@geodar.com>
2013-01-04 16:17:28 -0500

    Implement basic get temperature. Little ugly for now as it depends heavilly on the format of the /sys/bus/w1... file but string manipulation is not C's strongest feature :).

--- src/DS18B20.cpp
+++ src/DS18B20.cpp
@@ -23,12 +23,65 @@
 
 #include "DS18B20.h"
 
-DS18B20::DS18B20() {
-	// TODO Auto-generated constructor stub
-
+DS18B20::DS18B20(char* address) :
+		address_(address) {
+	unit_ = CELCIUS;
+	char path[47]; // path should be 46 chars
+	snprintf(path, 46, "%s%s%s", BUS, address_, TEMPFILE);
+	devFile = fopen(path, "r");
+	if (devFile == NULL) {
+		throw errno;
+	}
 }
 
 DS18B20::~DS18B20() {
-	// TODO Auto-generated destructor stub
+	fclose(devFile);
 }
 
+float DS18B20::getTemp() {
+	float temp = -1;
+	if (devFile != NULL) {
+		fseek(devFile, 0, SEEK_END);
+		if (!ferror(devFile)) {
+			size_t size = ftell(devFile);
+			char *filecontents = (char*) malloc(sizeof(char) * size);
+			rewind(devFile);
+			fread(filecontents, sizeof(char), size, devFile);
+
+			//here we depend on the file contents format
+			//This will likely break when/if they make a change
+			char* pch = strtok(filecontents, "\n"); // get first line
+			if (pch != NULL) {
+				if (strstr(pch, "YES") != NULL){ // if we have a YES in the crc proceed
+					pch = strtok(NULL, "\n"); // get second line
+					if (pch != NULL) {
+						char * dpch = strstr(pch, "="); // get the location of the temperature, probably constant but still...
+						if (dpch != NULL){
+							char tmp[6];
+							strncpy(tmp, dpch+1, 5); // store 5 digit temperature
+							temp = (float) atoi(tmp) / 1000.0; // we made it!
+						}
+					}
+				}
+			}
+			//cleanup
+			delete[] filecontents;
+		}
+	}
+	if (unit_ == CELCIUS)
+		return temp;
+	else
+		return CtoF(temp);
+}
+
+uint8_t DS18B20::getUnits(){
+	return unit_;
+}
+
+void DS18B20::setUnits(uint8_t u){
+	unit_ = u;
+}
+
+float DS18B20::CtoF(float temp){
+	return temp*1.8+32;
+}
--- src/DS18B20.h
+++ src/DS18B20.h
@@ -23,11 +23,29 @@
 
 #ifndef DS18B20_H_
 #define DS18B20_H_
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define CELCIUS 0
+#define FAHRENHEIT 1
+#define BUS "/sys/bus/w1/devices/"
+#define TEMPFILE "/w1_slave"
 
 class DS18B20 {
 public:
-	DS18B20();
+	DS18B20(char* address);
 	virtual ~DS18B20();
+	uint8_t getUnits();
+	void setUnits(uint8_t);
+	float getTemp();
+	float CtoF(float);
+private:
+	uint8_t unit_;
+	char* address_;
+	FILE *devFile;
 };
 
 #endif /* DS18B20_H_ */
