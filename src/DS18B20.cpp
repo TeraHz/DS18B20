e67e84aef596151021c9feaed42c5c75eefc724d
Georgi Todorov <terahz@geodar.com>
2013-01-03 21:31:18 -0500

    Initial commit

---
+++ src/DS18B20.cpp
@@ -0,0 +1,34 @@
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Name        : DS18B20.cpp
+ * Author      : Georgi Todorov <terahz@geodar.com>
+ * Version     : 
+ * Created on  : Jan 3, 2013
+ *
+ * Copyright © 2013 Georgi Todorov <terahz@geodar.com>
+ */
+
+#include "DS18B20.h"
+
+DS18B20::DS18B20() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DS18B20::~DS18B20() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/DS18B20.h
@@ -0,0 +1,33 @@
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Name        : DS18B20.h
+ * Author      : Georgi Todorov <terahz@geodar.com>
+ * Version     : 
+ * Created on  : Jan 3, 2013
+ *
+ * Copyright © 2013 Georgi Todorov <terahz@geodar.com>
+ */
+
+#ifndef DS18B20_H_
+#define DS18B20_H_
+
+class DS18B20 {
+public:
+	DS18B20();
+	virtual ~DS18B20();
+};
+
+#endif /* DS18B20_H_ */
